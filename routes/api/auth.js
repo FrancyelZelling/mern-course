1589f78d184a81f0e8532b94f47dc3fa4f536ac7
FrancyelZelling <zellingf@gmail.com>
2019-11-19 17:40:06 -0300

    Custom auth middleware & jwt verify

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,11 +3,22 @@ const express = require("express");
 // Router is required if you want to have routes in separeted files, not only on server.js
 const router = express.Router();
 
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+
 // When using router, insead of using express, you use the router from express to define your routes
 
 // @route GET api/auth
 // @desc  Test route
 // @acess Public
-router.get("/", (req, res) => res.send("Auth Route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json({ user });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
 
 module.exports = router;
